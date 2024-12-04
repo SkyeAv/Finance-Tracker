0b985a296acfa7bd46b59529a741fafd3264ee5e
SkyeAv <skye.a.goetz@gmail.com>
2024-12-04 12:07:50 -0800

    Skye Goetz 12/04/24

--- project.clj
+++ project.clj
@@ -7,7 +7,8 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/java.jdbc "0.7.12"]
-                 [org.xerial/sqlite-jdbc "3.42.0.0"]]
+                 [org.xerial/sqlite-jdbc "3.42.0.0"]
+                 [clj-time "0.5.0"]]
   :main ^:skip-aot finance-tracker.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
